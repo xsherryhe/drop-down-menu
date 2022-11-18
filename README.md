3e59cac860e1731e16281ef8c97255730eb6689a
Sherry He <xsherryhe@gmail.com>
2022-11-17 19:59:00 -0500

    Initial commit with drop-down menu code

---
+++ README.md
@@ -0,0 +1,6 @@
+To use this package:
+1) Use the dropDownMenu function by adding the following line at the top level of your code:
+  `import dropDownMenu from 'drop-down-menu';`
+
+2) Include the drop-down-menu.css stylesheet in your project. This is the file path from the root directory:
+  `node_modules/drop-down-menu/drop-down-menu.css`
---
+++ sample/drop-down-menu.js
@@ -0,0 +1,38 @@
+function dropDownMenu(menu) {
+  menu.classList.add('dropdown-menu');
+  const [menuHeading, ...menuItems] = menu.children;
+  menuHeading.classList.add('dropdown-menu-heading');
+
+  menuItems.forEach((menuItem) => {
+    menuItem.classList.add('dropdown-menu-item');
+  });
+
+  function slideUpMenuItems(targetMenu) {
+    targetMenu
+      .querySelectorAll('.dropdown-menu-item:not(.dropped-down)')
+      .forEach((menuItem, i) => {
+        menuItem.setAttribute(
+          'style',
+          `transform: translateY(-${(i + 1) * 100}%);`
+        );
+      });
+  }
+  slideUpMenuItems(menu);
+
+  function slideDownMenuItems(targetMenu) {
+    targetMenu
+      .querySelectorAll('.dropdown-menu-item.dropped-down')
+      .forEach((menuItem) => {
+        menuItem.setAttribute('style', 'transform: translateY(0);');
+      });
+  }
+
+  function toggleMenuItems() {
+    menuItems.forEach((menuItem) => {
+      menuItem.classList.toggle('dropped-down');
+      slideUpMenuItems(menu);
+      slideDownMenuItems(menu);
+    });
+  }
+  menu.addEventListener('click', toggleMenuItems);
+}
